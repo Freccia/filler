367ddbee55eb0626778595aa724c5d37a6535c92
Freccia <Freccia@users.noreply.github.com>
2016-11-10 18:24:11 +0100

    UP: libsdl deleted. gitignore updated

--- libsdl/build/.libs/SDL_audiodev.d
+++
@@ -1,9 +0,0 @@
-build/SDL_audiodev.lo: \
-  /Users/lfabbro/Cursus/filler/SDL2/src/audio/SDL_audiodev.c \
-  /Users/lfabbro/Cursus/filler/SDL2/src/audio/../SDL_internal.h \
-  /Users/lfabbro/Cursus/filler/SDL2/src/audio/../dynapi/SDL_dynapi.h \
-  /Users/lfabbro/Cursus/filler/SDL2/src/audio/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h \
-  /Users/lfabbro/Cursus/filler/SDL2/include/SDL_platform.h \
-  /Users/lfabbro/Cursus/filler/SDL2/include/begin_code.h \
-  /Users/lfabbro/Cursus/filler/SDL2/include/close_code.h
--- libsdl/build/SDL_render_d3d11.d
+++
@@ -1,9 +0,0 @@
-build/SDL_render_d3d11.lo: \
-  /Users/lfabbro/Cursus/filler/SDL2/src/render/direct3d11/SDL_render_d3d11.c \
-  /Users/lfabbro/Cursus/filler/SDL2/src/render/direct3d11/../../SDL_internal.h \
-  /Users/lfabbro/Cursus/filler/SDL2/src/render/direct3d11/../../dynapi/SDL_dynapi.h \
-  /Users/lfabbro/Cursus/filler/SDL2/src/render/direct3d11/../../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h \
-  /Users/lfabbro/Cursus/filler/SDL2/include/SDL_platform.h \
-  /Users/lfabbro/Cursus/filler/SDL2/include/begin_code.h \
-  /Users/lfabbro/Cursus/filler/SDL2/include/close_code.h
